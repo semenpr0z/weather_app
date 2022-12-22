8ad190ce71e9ec6292ea78c32e293a23f6cfcd68
Semen <justpr0z.com.ru@gmail.com>
2022-12-22 22:14:12 +0300

    added script.js

---
+++ script.js
@@ -0,0 +1,85 @@
+const apiKey = "586c092f9d86c7b5413a3412bb19e319";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?";
+
+// 1. Получать текущее время и показывать в интерфейсе
+const days = [
+    "Воскресенье",
+    "Понедельник",
+    "Вторник",
+    "Среда",
+    "Четверг",
+    "Пятница",
+    "Суббота"
+];
+
+const now = new Date();
+const day = days[now.getDay()];
+const hours = now.getHours();
+const minutes = now.getMinutes();
+
+let date = document.querySelector("#now");
+date.innerHTML = day + ", " + hours + ":" + minutes;
+
+// 2. Получать погоду по API для конкретного города
+function changeCity(event) {
+    event.preventDefault();
+
+    let currentCity = document.querySelector("#city");
+    let cityInput = document.querySelector("#searchInput");
+
+    currentCity.innerHTML = cityInput.value;
+
+    fetch(`${apiUrl}q=${cityInput.value}&appid=${apiKey}&units=metric`)
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+
+            let tempValue = document.querySelector("#temp");
+            tempValue.innerHTML = `${response.main.temp} ºC`;
+        })
+        .catch((error) => {
+            console.error(error);
+
+            let tempValue = document.querySelector("#temp");
+            tempValue.innerHTML = `Ошибка, дорогой пользователь, проверь свой код!`;
+        });
+}
+
+let search = document.querySelector("#button");
+search.addEventListener("click", changeCity);
+
+// 3. Получать погоду по API по местоположению пользователя
+function getCurrentPosition(event) {
+    event.preventDefault();
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        const lat = position.coords.latitude;
+        const lon = position.coords.longitude;
+
+        fetch(`${apiUrl}lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
+        .then((response) => response.json())
+        .then((response) => {
+            console.log(response);
+
+            let tempValue = document.querySelector("#temp");
+            tempValue.innerHTML = `${response.main.temp} ºC`;
+
+            let city = document.querySelector("#city");
+            city.innerHTML = "Current Location";
+        })
+        .catch((error) => {
+            console.error(error);
+
+            let tempValue = document.querySelector("#temp");
+            tempValue.innerHTML = `Ошибка, дорогой пользователь, проверь свой код!`;
+        });
+  });
+
+    let currentCity = document.querySelector("#city");
+    let cityInput = document.querySelector("#searchInput");
+
+    currentCity.innerHTML = cityInput.value;
+}
+
+let current = document.querySelector("#currentLocation");
+current.addEventListener("click", getCurrentPosition);
